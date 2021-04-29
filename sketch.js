2984f2d402e001939e8cde02dec7f721257aa11b
Rashmi Deshmukh <57807833+rashmi-deshmukh@users.noreply.github.com>
2021-04-30 01:12:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var score =0;
+var gun,bluebubble,redbubble, bullet, backBoard;
+var gunImg,bubbleImg, bulletImg, blastImg, backBoardImg;
+var redBubbleGroup, redBubbleGroup, bulletGroup;
+
+var life =3;
+var score=0;
+var gameState=1
+
+function preload(){
+  gunImg = loadImage("gun1.png")
+  blastImg = loadImage("blast.png")
+  bulletImg = loadImage("bullet1.png")
+  blueBubbleImg = loadImage("waterBubble.png")
+  redBubbleImg = loadImage("redbubble.png")
+  backBoardImg= loadImage("back.jpg")
+}
+function setup() {
+  createCanvas(800, 800);
+
+  backBoard= createSprite(50, width/2, 100,height);
+  backBoard.addImage(backBoardImg)
+  
+  gun= createSprite(100, height/2, 50,50);
+  gun.addImage(gunImg)
+  gun.scale=0.2
+  
+  bulletGroup = createGroup();   
+  blueBubbleGroup = createGroup();   
+  redBubbleGroup = createGroup();   
+  
+  
+}
+
+function draw() {
+  background("#BDA297");
+
+  //display Score and number of lifes
+
+  if(gameState===1){
+    gun.y=mouseY  
+
+    
+    drawSprites();
+  }
+     
+}
+
